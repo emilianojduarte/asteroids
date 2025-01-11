8e2823b1253efb08f031acdfce2d1f7e0ea72e50
emilianojduarte <emilianojduarte@gmail.com>
2025-01-11 12:18:55 -0300

    se mojora usando grupos para los objetos en pantalla

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
@@ -16,11 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
 
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
